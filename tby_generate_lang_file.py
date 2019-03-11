906130999950d9141ecc06a73a0b681264c50ab6
glitchroy <glitchroy@outlook.com>
2019-03-11 15:54:33 +0100

    Re-organize script into functions Add constants for folder names

--- tby_generate_lang_file.py
+++ tby_generate_lang_file.py
@@ -7,7 +7,10 @@ from tby_generate_object_names import create_object_names
 root = tk.Tk()
 root.withdraw()
 
-IDENTIFIER = "// Maintained by textboxy | Parser v0.1-beta"
+IDENTIFIER = "// Maintained by textboxy | tby-lang-tool v0.1-beta"
+OBJECT_FOLDER_NAME = "objects"
+ROOM_FOLDER_NAME = "rooms"
+
 
 def load_json(path):
     with open(path) as f:
@@ -15,47 +18,63 @@ def load_json(path):
     return data
 
 
+def save_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def get_instance_params(instance, object_names_dict):
+    params = {}
+    params["room_id"] = instance["name"]
+    params["object_type"] = object_names_dict[ instance["objId"] ]
+    params["dialogue"] = []
+
+    return params
+
+
+def process_layer(layer, object_names_dict):
+    layer_all_instances = []
+
+    if "instances" in layer and layer["instances"]: #if instances is not empty
+        for inst in layer["instances"]:
+            layer_all_instances.append( get_instance_params(inst, object_names_dict) )
+
+    return layer_all_instances
+
+
+def process_room(room, object_name_dict):
+    room_all_layers = {}
+
+    # loop through all layers in room
+    for layer in room["layers"]:
+        current_layer = process_layer(layer, object_name_dict)
+        if current_layer: #if current_layer is not [], e.g. there are instances
+            room_all_layers[layer["name"]] = current_layer
+        
+    return room_all_layers
+
+
 def generate_lang_file():
-    object_names = create_object_names(project_folder + "/objects")
+    object_names = create_object_names(project_folder + "/" + OBJECT_FOLDER_NAME)
 
     all_room_data = {}
 
-    for dirpath, dirnames, filenames in os.walk(project_folder + "/rooms"):
+    for dirpath, dirnames, filenames in os.walk(project_folder + "/" + ROOM_FOLDER_NAME):
         # for every .yy file in /rooms
         for filename in [f for f in filenames if f.endswith(".yy")]:
             full_path = os.path.join(dirpath, filename)
             room_json = load_json(full_path)
 
-            room_all_layers = {}
+            all_room_data[room_json["name"]] = process_room(room_json, object_names)
 
-            # loop through all layers in room
-            for layer in room_json["layers"]:
-                current_layer_all_instances = []
-                # loop through all instances in layer
-                if "instances" in layer and layer["instances"]: # if instances is not empty
-                    for inst in layer["instances"]:
-                        current_instance = {}
+    save_json(all_room_data, "lang.json")
 
-                        current_instance["room_id"] = inst["name"]
-                        current_instance["object_type"] = object_names[inst["objId"]]
-                        current_instance["dialogue"] = []
-
-                        current_layer_all_instances.append(current_instance)
-
-                    room_all_layers[layer["name"]] = current_layer_all_instances
-                
-            all_room_data[room_json["name"]] = room_all_layers
-
-    with open('lang.json', 'w') as f:
-        json.dump(all_room_data, f, indent=4)
-
-# execute
-
+########
+# start executing
 project_folder = filedialog.askdirectory(title = "Select project folder")
 
 # sanity checks
-if (os.path.isdir(project_folder + "/objects") and os.path.isdir(project_folder + "/rooms")):
+if (os.path.isdir(project_folder + "/" + OBJECT_FOLDER_NAME) and os.path.isdir(project_folder + "/" + ROOM_FOLDER_NAME)):
     generate_lang_file()
 else:
     print("Please specify a valid project folder.")
-
